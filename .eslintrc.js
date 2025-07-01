1a5a205be24d8af6f43a8c9208bfcfb8b6760e4d
idrise4112 <166262404+idrise4112@users.noreply.github.com>
2025-06-30 23:58:09 -0400

    new update

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+// console.log("asdf");
+console.error("asdf");
+
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": ["warn", { allow: ["error"] }],
+  },
+};
